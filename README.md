f06ba70b709230258df96ca0832b37602db1b3c6
Pedro Tavares Silva <ptavaressilva@gmail.com>
2022-04-17 14:37:10 +0200

    Accept and validate input values

--- README.md
+++ README.md
@@ -10,23 +10,35 @@ The application is written in Python, served with Flask and can the API can be c
 
 Training the ML model falls outside of the scope of this exercise, so we'll use a previously trained classifier model form my [Titanic GitHub repository](https://github.com/ptavaressilva/titanic).
 
-The API has a single endpoint called `/titanic`.
+The API has a single GET endpoint called `/titanic`.
 
-### Running the API on the local computer
+## Running the API on the local computer (development environment)
 
 1. In the 'env' directory run './build.sh' to create the Docker image.
-2. In the repository root run 'env/start_linux.sh' to start the container.
+2. In the repository root run 'dev_env/start_linux.sh' to start the container.
 
-The API will be exposed on port 6000 of the localhost.
+The API will be exposed on port 6000 of the localhost and will auto-reload whenever you change (and save) the 'api.py' program.
 
 ![Flask server running](img/0.02.png)
 
-You can use it with Postman (or another application of your choice) with GET on `http://localhost/titanic`
+You can use it with curl:
 
-![GET on endpoint /titanic](img/0.01.png)
+```shell
+curl --location --request GET 'http://localhost:6000/titanic' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "Age":"10",
+    "Fare": "123.0",
+    "SibSp":"2",
+    "Sex":"female",
+    "Cabin":"A",
+    "Embarked":"S"
+}'
+```
+You can also use Postman, or another application of your choice.
 
-### Current state of the API
+![GET on endpoint /titanic](img/0.04.png)
 
-In this version (0.1) the API will only return the string `{'survived': 'I will tell you soon'}`
+## Current state of the API
 
-Next version will require passing the values for Age', 'SibSp', 'Fare', 'Sex', 'Cabin' and 'Embarked' and will return the predicted survival of that passenger.
+As of this version (0.2) the API accepts the values required for the prediction of the survival of the passenger, and it validates that those values are valid. It does not however return a prediction of the survival of the (real or imaginary) Titanic passenger. It will only return the string `{'survived': 'I will tell you soon'}`
--- api.py
+++ api.py
@@ -1,36 +1,80 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-#import pandas as pd
-#from sklearn.preprocessing import OneHotEncoder, StandardScaler
+# import pandas as pd
+# from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
-#import pickle
+# import pickle
 
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('Age', required=True)
+parser.add_argument('Fare', required=True)
+parser.add_argument('SibSp', required=True)
+parser.add_argument('Sex', required=True)
+parser.add_argument('Cabin', required=True)
+parser.add_argument('Embarked', required=True)
+
 
 class Titanic(Resource):
-    def get(self):
+    def get(self):  # , SibSp, Fare, Sex, Cabin, Embarked
+        args = parser.parse_args()
 
-        # imput features:
-        #   'Age'
-        #   'SibSp'
-        #   'Fare'
-        #   'Sex' is in ['female', 'male']
-        #   'Cabin' is in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'W', 'Z']
-        #   'Embarked' is in ['C', 'Q', 'S']
+        # 'Age' must be float
+        try:
+            age = float(args['Age'])
+        except:
+            return {
+                'message': f"'{args['Age']}' is not a valid number of years (must be float or int)."
+            }, 200
 
-        # normalize 'Age', 'SibSp', 'Fare'
-     #       scaler = json.loads(scaler_json)
+        # 'SibSp' must be int
+        try:
+            SibSp = int(args['SibSp'])
+        except:
+            return {
+                'message': "'{}' is not a valid SibSp value (number of siblings and spouses aboard the Titanic must be integer).".format(args['SibSp'])
+            }, 200
 
-        # encode 'Sex', 'Cabin', 'Embarked'
-     #       encoder = json.loads(encoder_json)
+        # 'Fare' must be float
+        try:
+            Fare = float(args['Fare'])
+        except:
+            return {
+                'message': "'{}' is not a valid Fare in british pounds value (must be float or int).".format(args['Fare'])
+            }, 400
+
+        # 'Sex' is in ['female', 'male']
+        if args['Sex'] not in ['female', 'male']:
+            return {
+                'message': f"'{args['Sex']}' is not a valid Sex value (must be 'female' or 'male')."
+            }, 400
+
+        # 'Cabin' is in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'W', 'Z']
+        if args['Cabin'] not in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'W', 'Z']:
+            return {
+                'message': f"'{args['Cabin']}' is not a valid Cabin letter (must be 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'T', 'W' or 'Z')."
+            }, 400
+
+        # 'Embarked' is in ['C', 'Q', 'S']
+        if args['Embarked'] not in ['C', 'Q', 'S']:
+            return {
+                'message': f"'{args['Embarked']}' is not a valid port of departure letter (must be 'C', 'Q' or 'S')."
+            }, 400
+
+    #   normalize 'Age', 'SibSp', 'Fare'
+    #         scaler = json.loads(scaler_json)
+
+    #   encode 'Sex', 'Cabin', 'Embarked'
+    #         encoder = json.loads(encoder_json)
 
         # return data and 200 OK
         return {'survived': 'I will tell you soon'}, 200
 
 
-api.add_resource(Titanic, '/titanic')  # add endpoints
+api.add_resource(Titanic, '/titanic')  # add endpoint
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)  # run our Flask app
+    # run our Flask app in development
+    app.run(host='0.0.0.0', port=6000, debug=True)
