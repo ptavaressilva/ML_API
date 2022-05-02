19c6bcbaf0b40acd79d69bb4e3c77b037a0831f2
Pedro Tavares Silva <ptavaressilva@gmail.com>
2022-05-02 15:56:26 +0200

    Additional tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -7,6 +7,7 @@ This module contains test cases for apy.py
 # https://stackoverflow.com/questions/7580532/how-to-check-whether-a-method-exists-in-python
 
 import api
+from pathlib import Path
 
 
 def test_get_method():
@@ -21,3 +22,9 @@ def test_options_method():
     # OPTIONS method
     assert hasattr(api.Titanic, 'options')
     assert callable(getattr(api.Titanic, 'options'))
+
+
+def test_ML_files_exist():
+    assert Path("DecisionTreeClassifier.pkl").is_file()
+    assert Path("encoder.pkl").is_file()
+    assert Path("scaler.pkl").is_file()
